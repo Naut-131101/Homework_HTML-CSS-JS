86fda67ed56f3f90ba24da3ae66b99c3103d502b
Huỳnh Nghiệp Tuấn <naut.131101@gmail.com>
2026-01-28 16:55:37 +0700

    Delete Exercise-JS-1/Ex1_2 directory

--- Exercise-JS-1/Ex1_2/two.js
+++
@@ -1,7 +0,0 @@
-function isLandscape(width, height) {
-    if (width > height) return true;
-    if (height > width) return false;
-}
-
-console.log(isLandscape(1, 2));
-console.log(isLandscape(3, 1));
